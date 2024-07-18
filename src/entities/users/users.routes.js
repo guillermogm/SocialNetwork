0b3e5b717eeaf8a242e69b3f82ca5e48e013dfcd
guillermogm <guillermogm009@gmail.com>
2024-07-18 21:05:16 +0200

    feat: Delete user by id controller

--- src/entities/users/users.routes.js
+++ src/entities/users/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, getUserProfile, updateUserProfile } from "./users.controller.js";
+import { getAllUsers, getUserProfile, updateUserProfile, deleteUser } from "./users.controller.js";
 import {auth} from "../../middleware/auth.js"
 import { isSuperAdmin } from "../../middleware/isSuperAdmin.js";
 
@@ -8,5 +8,6 @@ const router= Router()
 router.get("/", auth,isSuperAdmin, getAllUsers)
 router.get("/profile", auth, getUserProfile)
 router.put("/profile", auth, updateUserProfile)
+router.delete("/:id",auth,isSuperAdmin, deleteUser)
 
 export {router}
