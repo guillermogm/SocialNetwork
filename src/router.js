d8a8808859ef0bc7d685ba138b4fee481d823976
guillermogm <guillermogm009@gmail.com>
2024-07-19 22:02:02 +0200

    feat: Create post, Delete post and Update post controller

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { router as authRoutes } from "./entities/auth/auth.routes.js";
 import { router as userRoutes} from "./entities/users/users.routes.js";
+import { router as postRoutes} from "./entities/posts/posts.routes.js";
 
 const router= Router()
 
 router.use("/auth", authRoutes)
 
 router.use("/users", userRoutes)
+
+router.use("/posts", postRoutes)
 
 export {router}
